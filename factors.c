77b7ae3d483100fb5350058ca2a2b55ad1b07550
Fatimah 1403 <fattyhassan14@gmail.com>
2023-03-25 02:29:22 +0000

    Factors file added

---
+++ factors.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+	long long int n = 230980320525926;
+	long int p = 2;
+	long int q;
+
+	while (n % p)
+	{
+		if (p <= n)
+		{
+			p++;
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+
+	q = n / p;
+	print("%lld = %ld\n", n, p, q);
+	return (0);
+}
